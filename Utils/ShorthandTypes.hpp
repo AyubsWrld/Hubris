7b6c1d19d496f5efd1f3f987bf4a0ba9e4938886
AyubsWrld <ayubfmohammed@gmail.com>
2026-03-01 23:45:47 -0700

    more functional changes + util additions

---
+++ Utils/ShorthandTypes.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <utility>
+#include <cstdint>
+
+using   Comments    =   std::vector<std::string>;
+using   U64         =   std::uint64_t;
+using   NumericRange =  std::pair<U64, U64>;

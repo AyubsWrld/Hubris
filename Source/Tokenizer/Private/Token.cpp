2948e8d71bf62f072a404fb876f3a41502cc6da9
AyubsWrld <ayubfmohammed@gmail.com>
2026-03-07 23:01:42 -0700

    Adding stubs to Type functions

---
+++ Source/Tokenizer/Private/Token.cpp
@@ -0,0 +1,77 @@
+#pragma once
+#include "Token.hpp"
+#include "Construction.hpp";
+
+constexpr bool FToken::IsIdentifier() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+
+constexpr bool FToken::KeywordOrIdentifierIsKeyword() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+
+constexpr bool FToken::IsLiteralPropertyName() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+
+constexpr bool FToken::PrefixesExpression() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+constexpr bool FToken::IsAssignment() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+constexpr bool FToken::IsFlowInterfaceOrTypeOrOpaque() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+constexpr bool FToken::IsLoop() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+constexpr bool FToken::IsOperator() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+constexpr bool FToken::IsPostfix() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+constexpr bool FToken::IsPrefix() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+constexpr bool FToken::IsTSTypeOperator() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+constexpr bool FToken::IsTSDeclarationStart() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+constexpr bool FToken::IsRightAssociative() noexcept
+{
+    STUB_INVOCATION();
+    return false;
+}
+constexpr bool FToken::operator==(const FToken& other)
+{
+    STUB_INVOCATION();
+    return false;
+}
